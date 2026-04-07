f8d07994098bc57e087da55790009b7f118f432a
Luis Machado <luis.machado@amd.com>
2026-04-07 11:12:10 -0500

    Validate debug-tools for runtime changes
    Given rocr-debug-agent and rocgdb rely on the runtime, make sure to test
    those components whenever there are runtime changes.

--- .github/scripts/therock_matrix.py
+++ .github/scripts/therock_matrix.py
@@ -67,9 +67,10 @@ project_map = {
         "cmake_options": ["-DTHEROCK_ENABLE_ALL=OFF", "-DTHEROCK_ENABLE_ROCSHMEM=ON"],
         "projects_to_test": "",  # rocshmem testing to be enabled in a future PR
     },
+    # Also test rocr-debug-agent and rocgdb since those depend on runtimes.
     "runtimes": {
         "cmake_options": ["-DTHEROCK_ENABLE_ALL=ON"],
-        "projects_to_test": "hip-tests, rocrtst",
+        "projects_to_test": "hip-tests, rocrtst, rocr-debug-agent, rocgdb",
     },
     "all": {
         "cmake_options": ["-DTHEROCK_ENABLE_ALL=ON"],
