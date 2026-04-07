84d8d362dc42807e4ce2f6bd1676866d1756ca7d
Parth Ashwin Jain <parthash@amd.com>
2026-04-07 03:01:23 -0700

    Fix after merge conflict
    Signed-off-by: Parth Ashwin Jain <parthash@amd.com>

--- projects/clr/rocclr/device/device.hpp
+++ projects/clr/rocclr/device/device.hpp
@@ -696,7 +696,7 @@ class Settings : public amd::HeapObject {
       uint kernel_arg_opt_ : 1;               //!< Enables kernel arg optimization for blit kernels
       uint kernel_arg_impl_ : 2;              //!< Kernel argument implementation
       uint sdma_swap_supported_ : 1;         //!< SDMA linear swap copy (gfx94x/gfx95x)
-      uint reserved_ : 13;
+      uint reserved_ : 12;
     };
     uint value_;
   };
